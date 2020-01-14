905776ee7d46b5690fe3913959ef357f460dc05a
Vladan Jovic <vladan.jovic@codeplicity.com>
2020-01-14 14:12:12 +0100

    Send email via MailerQ REST

--- src/Mailer.php
+++ src/Mailer.php
@@ -4,6 +4,7 @@ namespace G4\Mailer;
 
 use G4\Mailer\Transport\Amazon\AmazonSes;
 use G4\Mailer\Transport\Smtp\Smtp;
+use G4\Mailer\Transport\MailerQ\Rest\MailerQ;
 use G4\Mailer\Transport\TransportInterface;
 
 class Mailer implements  MailerInterface
@@ -26,12 +27,13 @@ class Mailer implements  MailerInterface
     public static function factory($options)
     {
         if (!isset($options['delivery'])) {
-            throw new \InvalidArgumentException('Key "delivery" not specified, should be smtp, amazon_sess, etc...');
+            throw new \InvalidArgumentException('Key "delivery" not specified, should be smtp, amazon_sess, mailerq_rest, etc...');
         }
 
         switch ($options['delivery']) {
-            case 'smtp';       $transport = new Smtp($options); break;
-            case 'amazon_ses'; $transport = new AmazonSes($options); break;
+            case 'smtp';         $transport = new Smtp($options); break;
+            case 'amazon_ses';   $transport = new AmazonSes($options); break;
+            case 'mailerq_rest'; $transport = new MailerQ($options); break;
             default:
                 throw new \Exception("Mail delivery not defined");
         }
---
+++ src/Transport/MailerQ/Rest/MailerQ.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace G4\Mailer\Transport\MailerQ\Rest;
+
+use G4\Mailer\Client\CurlHttpClient;
+use G4\Mailer\Transport\TransportInterface;
+use G4\Mailer\Message\MailerQ\Rest\MessageFacade;
+
+class MailerQ implements TransportInterface
+{
+    private $options;
+
+    public function __construct($options)
+    {
+        $this->setOptions($options);
+    }
+
+    public function send(\G4\Mailer\Message $message)
+    {
+        try {
+            $mailerQMessage = MessageFacade::convert($message, $this->options);
+
+            (new CurlHttpClient())->post($mailerQMessage->getBody(), $mailerQMessage->getHeaders(), $mailerQMessage->getUrl());
+        } catch (\Exception $exception) {
+            if ($exception->getMessage() !== sprintf('Empty response from %s', $this->options['params']['url'])) {
+                throw  new \RuntimeException(sprintf('Email not sent. Reason: %s', $exception->getMessage()));
+            }
+        }
+    }
+
+    private function setOptions($options)
+    {
+        if (!isset($options['params']['url'])) {
+            throw new \InvalidArgumentException('url not defined');
+        }
+
+        if (!isset($options['params']['token'])) {
+            throw new \InvalidArgumentException('token not defined');
+        }
+
+        $this->options = $options;
+    }
+}
